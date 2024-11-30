7d6cda15f7702c4f0103ee56c5417cf8ecd9c668
jiwonkim <andrewkimswe@gmail.com>
2024-11-30 10:07:35 +0900

    fix: quiz db

--- src/main/java/com/mosabulgyeo/bewavoca/dto/QuizResponse.java
+++ src/main/java/com/mosabulgyeo/bewavoca/dto/QuizResponse.java
@@ -43,8 +43,8 @@ public class QuizResponse {
 		/**
 		 * OX 퀴즈 고유 ID.
 		 */
-		@NotNull(message = "Quiz ID is required.")
-		private Long id;
+		@NotNull(message = "OX ID is required.")
+		private Long oxId;
 
 		/**
 		 * OX 퀴즈 질문.
@@ -105,18 +105,31 @@ public class QuizResponse {
 	public static class ChoiceQuiz {
 
 		/**
-		 * 표준어 단어.
-		 * 예: "바나나"
+		 * 4지선다형 퀴즈 고유 ID.
 		 */
-		@NotBlank(message = "Standard word is required.")
-		private String standard;
+		@NotBlank(message = "ChoiceId is required.")
+		private String choiceId;
+
+		/**
+		 * 4지 선다 질문.
+		 * 예: "___는 노란색이고 너무 달콤한 과일이야."
+		 */
+		@NotBlank(message = "Question is required.")
+		private String question;
+
+		/**
+		 * 정답 선택지 배열.
+		 * 예: ["바나나", "사과", "키위", "포도"]
+		 */
+		@NotNull(message = "Options are required.")
+		private String[] options;
 
 		/**
 		 * 정답 제주어 단어.
-		 * 예: "A"
+		 * 예: "바나나"
 		 */
-		@NotBlank(message = "Jeju word is required.")
-		private String jeju;
+		@NotBlank(message = "Correct answer is required.")
+		private String correctAnswer;
 
 		/**
 		 * 정답 설명.
@@ -127,6 +140,7 @@ public class QuizResponse {
 
 		/**
 		 * 음성 파일 경로.
+		 * 예: "link_to_voice_file"
 		 */
 		@NotBlank(message = "Voice file path is required.")
 		private String voice;
